b646ea1b4c42b43d87e36fd87ca6eae2211ccc51
Alexandr Virgovič <91419390+devdeem@users.noreply.github.com>
2025-01-24 14:59:21 +0100

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,9 @@
-"use client";
-
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
 	return {
 		rules: {
-			userAgent: "",
+			userAgent: "*",
 			allow: "/",
 		},
 	};
