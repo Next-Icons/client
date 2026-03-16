63ee20c5597fe6ffb22aa597a29c3e23aaf3c226
Alexandr Virgovič <91419390+devdeem@users.noreply.github.com>
2026-03-16 13:39:57 +0100

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,3 +5,4 @@ export const GITHUB_REPO_NAME = "Next-Icons/next-icons" as const;
 export const GITHUB_REPO_URL = `https://github.com/${GITHUB_REPO_NAME}` as const;
 
 export const NPM_PACKAGE_URL = "https://www.npmjs.com/package/@deemlol/next-icons" as const;
+export const X_URL = "https://x.com/NextIcons" as const;
